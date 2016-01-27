fdbb08d3460225296377ff9dc3cc50a35328f4b6
browneryan <browneryan87@gmail.com>
2016-01-26 22:05:35 -0800

    Add price prototype to scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,15 @@ function Pizza(pizzaSize, pizzaToppings, pizzaQuantity) {
   this.pizzaToppings = pizzaToppings;
   this.pizzaQuantity = pizzaQuantity;
 }
+
+Pizza.prototype.price = function() {
+  debugger;
+  var price = 10;
+  if (this.pizzaSize === "large") {
+    price += 10;
+  } else if (this.pizzaSize === "medium") {
+    price += 5;
+  } else {
+
+  } return price;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -7,15 +7,15 @@
 
 describe('Pizza', function(){
   it("creates new pizza with given properties", function() {
-    var newPizza = new Pizza("large","artichokes",5);
+    var newPizza = new Pizza("large", "artichokes", 5);
     expect(newPizza.pizzaSize).to.equal("large");
     expect(newPizza.pizzaToppings).to.equal("artichokes");
     expect(newPizza.pizzaQuantity).to.equal(5);
   });
 });
 describe('Price', function(){
-  it("calculates price based on property values", function(){
-    var newPrice = new Pizza("large","artichokes",5);
-    expect(newPrice()).to.equal(20);
+  it("calculates price based on property values", function() {
+    var newPrice = new Pizza("large", "artichokes", 5);
+    expect(newPrice.price()).to.equal(20);
   });
 });
