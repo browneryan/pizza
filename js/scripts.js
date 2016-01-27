9171acd12823534c69e8285e4edf74d149df388b
browneryan <browneryan87@gmail.com>
2016-01-26 22:06:28 -0800

    Add price prototype to scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-// Uncomment this to see the example spec test pass. Delete if you don't need it!
-// var helloWorld = function(){
-//   return false;
-// };
 function Pizza(pizzaSize, pizzaToppings, pizzaQuantity) {
   this.pizzaSize = pizzaSize;
   this.pizzaToppings = pizzaToppings;
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,3 @@
-// Example spec. Uncomment if you want to see it run in spec-runner.html! Then uncomment the function in scripts.js
-// describe('helloWorld', function(){
-//   it("is going to be false", function(){
-//     expect(helloWorld()).to.equal(false);
-//   });
-// });
-
 describe('Pizza', function(){
   it("creates new pizza with given properties", function() {
     var newPizza = new Pizza("large", "artichokes", 5);
